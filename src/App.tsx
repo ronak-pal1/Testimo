1071c770f6d2b34b6cc027fc2e73deaecbd2c6f5
ronak-pal1 <ronakpaul882@gmail.com>
2024-10-05 20:57:36 +0530

    routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
-function App() {
-  return <></>;
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Homepage from "./pages/Homepage";
+import NotFound from "./pages/NotFound";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Homepage />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
